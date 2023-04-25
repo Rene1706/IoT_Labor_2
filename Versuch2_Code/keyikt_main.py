0cf73567a59c38263cefeeef9de3cfef2bfb8529
Rene1706 <rene.huertgen@gmx.de>
2023-04-25 23:00:28 +0200

    Added code for up, down, reset button to change velocity over time via calculated accel, friction per delta timestep

--- Versuch2_Code/keyikt_main.py
+++ Versuch2_Code/keyikt_main.py
@@ -5,16 +5,18 @@
 #######################################################################
 
 import pygame
+import math
 
 width = 400
 height = 200
 
 freq = 50  # Sets the frequency of input procession
 delta = 1.0 / freq # time per step
-acc = 2.6  # Max acceleration of the car (per sec.)
+acc_max = 2.6  # Max acceleration of the car (per sec.)
 dec = 4.5  # Max deceleration of the car (per sec.)
-frict = -1  # max friction
+frict_max = -1.0  # max friction
 angle_acc = 300  # max change of angle (per sec.)
+vmax = 11.0
 
 speed_cur = 0
 angle_cur = 0
@@ -31,7 +33,19 @@ clock = pygame.time.Clock()
 
 
 # States of the keys
-keystates = {'quit':False}
+keystates = {'quit': False, 'up': False, 'down': False, 'reset': False}
+
+def calculateAcceleration(velocity, vmax, acc_max):
+    µ = vmax/2.0
+    sigma = 2.5
+    acceleration = acc_max*(1-1.0/2.0*(1+math.erf((abs(velocity)-µ)/(math.sqrt(2*sigma**2)))))
+    return acceleration
+
+def calculateFriction(velocity, vmax, frict_max):
+    µ = vmax/2.0
+    sigma = 4.0
+    friction = frict_max/2.0*(1+math.erf((abs(velocity)-µ)/(math.sqrt(2*sigma**2))))
+    return friction
 
 running = True
 try:
@@ -53,20 +67,52 @@ try:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     keystates['quit'] = True
+                elif event.key == pygame.K_UP:
+                    keystates['up'] = True
+                elif event.key == pygame.K_DOWN:
+                    keystates['down'] = True
+                elif event.key == pygame.K_r:
+                    keystates['reset'] = True
 
             # check for key up events (release)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_q:
                     keystates['quit'] = False
+                elif event.key == pygame.K_UP:
+                    keystates['up'] = False
+                elif event.key == pygame.K_DOWN:
+                    keystates['down'] = False
+                elif event.key == pygame.K_r:
+                    keystates['reset'] = False
 
         # do something about the key states here, now that the event queue has been processed
         if keystates['quit']:
             running = False
+    
+        if keystates['reset']:
+            speed_cur = 0
+        
+        # Calculate acceleration and friction depending on velocity
+        acc = calculateAcceleration(speed_cur, vmax, acc_max)
+        frict = calculateFriction(speed_cur, vmax, frict_max)
+
+        #TODO Check that keydown is negativ acceleration when speed < 0 
+        if keystates['up'] and not keystates['down']:
+            if speed_cur < vmax:
+                speed_cur += acc * delta
+        elif keystates['down'] and not keystates['up']:
+            if speed_cur > -vmax:
+                speed_cur -= dec * delta
+        else:
+            if speed_cur > 0:
+                speed_cur += frict * delta
+            elif speed_cur < 0:
+                speed_cur -= frict * delta
         
         print("({},{} --> {})".format(speed_cur, angle_cur, (speed_cur - last) / delta))
     
 except KeyboardInterrupt:
-    print "Exiting through keyboard event (CTRL + C)"
+    print ("Exiting through keyboard event (CTRL + C)")
     
 # gracefully exit pygame here
 pygame.quit()
