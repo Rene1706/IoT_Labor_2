623c9d56081a0d67166b70c577466648cac9f31a
Rene1706 <rene.huertgen@gmx.de>
2023-05-05 20:17:30 +0200

    Added speed, angle control with wii keys

--- Versuch2_Code/wiikt_main.py
+++ Versuch2_Code/wiikt_main.py
@@ -10,16 +10,22 @@ from linuxWiimoteLib import *
 wiimote = Wiimote()
 
 #Insert address and name of device here
-device = ('', '')
+#device = ('00:1F:C5:49:AE:86', 'Nintendo RVL-CNT-01')
+device = ('00:1F:C5:45:E0:A5', 'Nintendo RVL-CNT-01')
 
 connected = False
+speed_max = 11.0
+angle_max = 45.0
+cur_speed = 0
+cur_angle = 0
+dividor = 10.0
 
 try:
-	print "Press any key on wiimote to connect"
+	print("Press any key on wiimote to connect")
 	while (not connected):
 		connected = wiimote.Connect(device)
 
-	print "succesfully connected"
+	print("succesfully connected")
 
 	wiimote.SetAccelerometerMode()
 
@@ -27,10 +33,27 @@ try:
 	while True:
 		# re-calibrate accelerometer
 		if (wiistate.ButtonState.Home):
-			print 're-calibrating'
+			print('re-calibrating')
 			wiimote.calibrateAccelerometer()
-			sleep(0.1)			
+
+
+		# Handle Speed angle with keys
+		if (wiistate.ButtonState.Up):
+			if cur_angle > -angle_max:
+				cur_angle -= angle_max/dividor
+		if (wiistate.ButtonState.Down):
+			if cur_angle < angle_max:
+				cur_angle += angle_max/dividor
+		if (wiistate.ButtonState.Right):
+			if cur_speed <= speed_max:
+				cur_speed += speed_max/dividor
+		if (wiistate.ButtonState.Left):
+			if cur_speed >= -speed_max:
+				cur_speed -= speed_max/dividor
+
+		print("{},{}".format(cur_speed, cur_angle))
+		sleep(0.1)
 
 except KeyboardInterrupt:
-	print "Exiting through keyboard event (CTRL + C)"
+	print("Exiting through keyboard event (CTRL + C)")
 	exit(wiimote)
