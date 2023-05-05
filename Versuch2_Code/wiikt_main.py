a7261d759707f0e98275950310721341a3b0a534
Rene1706 <rene.huertgen@gmx.de>
2023-05-05 20:48:55 +0200

    Added handling for button one,two and led speed handling

--- Versuch2_Code/wiikt_main.py
+++ Versuch2_Code/wiikt_main.py
@@ -20,6 +20,25 @@ cur_speed = 0
 cur_angle = 0
 dividor = 10.0
 
+def setLeds(wiimote, cur_speed, speed_max):
+	limits = speed_max/4.0
+	led1 = False
+	led2 = False
+	led3 = False
+	led4 = False
+	if cur_speed == 0:
+		pass
+	elif cur_speed > 0:
+		led1 = True
+	if cur_speed > limits:
+		led2 = True
+	if cur_speed > limits*2:
+		led3 = True
+	if cur_speed > limits*3:
+		led4 = True
+	print("{},{},{},{}".format(led1,led2,led3,led4))
+	wiimote.SetLEDs(led1, led2, led3, led4)
+
 try:
 	print("Press any key on wiimote to connect")
 	while (not connected):
@@ -45,13 +64,22 @@ try:
 			if cur_angle < angle_max:
 				cur_angle += angle_max/dividor
 		if (wiistate.ButtonState.Right):
-			if cur_speed <= speed_max:
+			if cur_speed < speed_max:
 				cur_speed += speed_max/dividor
 		if (wiistate.ButtonState.Left):
-			if cur_speed >= -speed_max:
+			if cur_speed > 0:
 				cur_speed -= speed_max/dividor
 
-		print("{},{}".format(cur_speed, cur_angle))
+		# Drive forward/backwards depending on key 1/2
+		real_speed = 0	# +- current speed dependent on direction
+		if (wiistate.ButtonState.Two):
+			real_speed = cur_speed
+		if (wiistate.ButtonState.One):
+			real_speed = -cur_speed
+		
+		setLeds(wiimote, cur_speed, speed_max)
+
+		print("{},{}".format(real_speed, cur_angle))
 		sleep(0.1)
 
 except KeyboardInterrupt:
